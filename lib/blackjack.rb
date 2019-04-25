b7f30deb8ad0f120a04e89a607d5befe72f389b1
Tanuka Das <das.tanuka@yahoo.com>
2019-04-25 23:37:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,49 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
+
 end
 
 def deal_card
   # code #deal_card here
+  rand(1...11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  card_total > 7
+  puts "Your cards add up to #{card_total}"
+
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  value = gets.chomp
+  return value
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  return sum
 end
 
 def hit?
   # code hit? here
+  prompt_user
+  get_user_input
+  return 
 end
 
 def invalid_command
@@ -41,4 +57,3 @@ end
 def runner
   # code runner here
 end
-    
